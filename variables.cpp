1ca9adbfbd7565da04aeb749474fcc7b208791dc
Maheen <maheenbilalkk0@gmail.com>
2026-02-05 18:36:34 +0500

    Add files via upload

---
+++ variables.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int age = 18;
+    float height = 5.4;
+    char grade = 'A';
+    bool isStudent = true;
+    string name = "Maheen";
+
+    cout << "Name: " << name << endl;
+    cout << "Age: " << age << endl;
+    cout << "Height: " << height << endl;
+    cout << "Grade: " << grade << endl;
+    cout << "Is Student: " << isStudent << endl;
+
+    return 0;
+}
